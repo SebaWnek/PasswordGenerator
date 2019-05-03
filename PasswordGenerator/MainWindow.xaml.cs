bdac101cb0dd533d42c429066ef1a0864a995a22
Sebastian Wnęk <kontakt@wnekofoty.pl>
2019-05-03 10:58:09 +0200

    Dodaj pliki projektów.

---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        internal string password = "";
+        Random rnd = new Random();
+        List<string> letters = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            letters.Clear();
+            password = ((char)rnd.Next(65, 91)).ToString();
+            for (int i = 0; i < 11; i++)
+            {
+                password += ((char)rnd.Next(97,123)).ToString();
+            }
+            password += "1!";
+            passwordBox.Text = password;
+            letters = password.ToList();
+            listBox1.ItemsSource = letters;
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(password);
+        }
+
+    }
+
+    internal static class PassLetter
+    {
+        static Dictionary<char, string> baseLetters = new Dictionary<char, string>()
+        {
+            { 'a', "ALFA" },
+            { 'b', "BRAVO" },
+            { 'c', "CHARLIE" },
+            { 'd', "DELTA" },
+            { 'e', "ECHO" },
+            { 'f', "FOXTROT" },
+            { 'g', "GOLF" },
+            { 'h', "HOTEL" },
+            { 'i', "INDIA" },
+            { 'j', "JULIET" },
+            { 'k', "KILO" },
+            { 'l', "LIMA" },
+            { 'm', "MIKE" },
+            { 'n', "NOVEMBER" },
+            { 'o', "OSCAR" },
+            { 'p', "PAPA" },
+            { 'q', "QUEBEC" },
+            { 'r', "ROMEO" },
+            { 's', "SIERRA" },
+            { 't', "TANGO" },
+            { 'u', "UNIFORM" },
+            { 'v', "VICTOR" },
+            { 'w', "WHISKEY" },
+            { 'x', "XRAY" },
+            { 'y', "YANKEE" },
+            { 'z', "ZULU" },
+            { '1', "Number ONE" },
+            { '!', "Exclamation mark" },
+        };
+
+        static Dictionary<char, string> alternateLetters = new Dictionary<char, string>()
+        {
+            { 'a', "Apple"},
+            { 'b', "Banana"},
+            { 'c', "Chocolate"},
+            { 'd', "Donald"},
+            { 'e', "England"},
+            { 'f', "Football"},
+            { 'g', "Germany"},
+            { 'h', "Hong-Kong"},
+            { 'i', "Indigo"},
+            { 'j', "Japan"},
+            { 'k', "Kite"},
+            { 'l', "London"},
+            { 'm', "Mumbai"},
+            { 'n', "New, No"},
+            { 'o', "Orange"},
+            { 'p', "Pakistan"},
+            { 'q', "Queen"},
+            { 'r', "Robert"},
+            { 's', "Snake, Singapore"},
+            { 't', "Television"},
+            { 'u', "Umbrella"},
+            { 'v', "Value, Virtual"},
+            { 'w', "Washington"},
+            { 'x', "Xenomorph"},
+            { 'y', "Yellow"},
+            { 'z', "Zebra"},
+            { '1', "Just the number"},
+            { '!', "Shift plus key one"},
+        };
+        
+        static string NameFromLetter(this char c)
+        {
+            string letter = c.ToString();
+            string result = letter.ToUpper();
+            result += " for ";
+            result += baseLetters[letter.ToLower()[0]];
+            result += " or ";
+            result += alternateLetters[letter.ToLower()[0]];
+            return result;
+        }
+
+        public static List<string> ToList(this string password)
+        {
+            List<string> result = new List<string>();
+            foreach (char c in password)
+            {
+                if(c < 91 && c > 64)
+                {
+                    result.Add("Capital " + c.NameFromLetter());
+                }
+                else
+                {
+                    result.Add(c.NameFromLetter());
+                }
+            }
+            return result;
+        }
+    }
+}
